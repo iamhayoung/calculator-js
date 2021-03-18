2d9106535bb679cccdaef87e02b9d8cda01122f8
Hayoung Gam <hahhyoung@gmail.com>
2021-03-18 21:27:35 +0900

    연산자 두번 누르면 입력안되게 제어

--- app.js
+++ app.js
@@ -1,33 +1,46 @@
 const display = document.querySelector('#js-display');
 const buttons = Array.from(document.querySelectorAll('button'));
 
-// 값 담는곳
-let memory = "";
-
-// 계산하는거
-const compute = () => {
-  memory = (new Function('return ' + memory))();
-}
+let memory = ""; // 값 담는곳
+let checkOperator = true;
 
 // 연산자 누르면 연산자를 memory에 담음
 const appendOperator = (operator) => {
+  // checkOperator가 true일때는 연산자 입력안되게함
+  if (checkOperator) {
+    return;
+  }
+  console.log(`appendOperator: ${memory}`)
   memory += operator;
+  checkOperator = true;
+  //곱하기 할때는 =버튼 안눌러도 결과가 떠야함
 }
 
 // 숫자 누르면 숫자를 memory에 담음
 const appendNumber = (num) => {
+  // 숫자입력할때는 checkOperator를 false로 해서 연산자 입력가능하게 함
+  checkOperator = false;
   memory += num;
+  console.log(`appendNumber: ${memory}`)
 }
 
-// memory에 담긴 값을 출력
-const updateDisplay = () => {
+// memory에 담긴 결과값을 출력
+const outputResult = () => {
   display.value = memory;
+  console.log(`outputResult: ${memory}`)
+}
+
+// 계산하는거
+const compute = () => {
+  memory = (new Function('return ' + memory))();
+  console.log(`compute: ${memory}`)
 }
 
 // memory를 빈칸으로 만들어주면서 클리어하는거
 const clearDisplay = () => {
   display.value = "0";
   memory = "";
+  checkOperator = true;
 }
 
 buttons.forEach(button => {
@@ -43,12 +56,12 @@ buttons.forEach(button => {
         break;
       case 'equals':
         compute();
-        updateDisplay()
+        outputResult()
         break;
       default:
         let num = parseInt(event.target.innerText);
         appendNumber(num);
-        updateDisplay();
+        outputResult();
     }
   })
 })
